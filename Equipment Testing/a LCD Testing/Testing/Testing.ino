dabe7bb7778e25f053b9d7161732419573b85bd2
Sergio Escuder <user@server>
2012-09-07 15:43:12 +0300

    LCD example

---
+++ Equipment Testing/a LCD Testing/Testing/Testing.ino
@@ -0,0 +1,27 @@
+//DFRobot.com
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                            // initialize the lcd 
+  lcd.backlight();                       // turn on backlight
+  lcd.print("*ArduinoHomeMonitor*");      // Print a message to the LCD.
+  
+  delay(5000);
+}
+
+void loop()
+{
+  lcd.setCursor(0, 0);
+	lcd.print("*ArduinoHomeMonitor*");
+  lcd.setCursor(0, 1);
+	lcd.print("millis(): ");lcd.print(millis());
+  lcd.setCursor(0, 2);
+	lcd.print("-------------------");
+  lcd.setCursor(0, 3);
+	lcd.print(" By: Sergio Escuder");
+
+}
